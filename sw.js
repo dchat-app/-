39e08758d2a837302da0b7e4736d2c04525bfb74
DCHAT <d.chat2345@gmail.com>
2026-04-30 22:16:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,23 @@
-const CACHE_NAME = "dchat-cache-v1";
+const CACHE_NAME = 'pwa-test-v1';
+const ASSETS = [
+    'index.html',
+    'manifest.json'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./logo.png"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
+// Install Event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
         })
-      )
-    )
-  );
+    );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+// Fetch Event (Allows offline viewing)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
